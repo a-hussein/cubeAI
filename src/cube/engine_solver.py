b47bfb3d8751f3f7288312ac267fad41a7d49662
Salem Abuammer <sabuammer@gmail.com>
2026-02-14 22:34:45 -0600

    refactor

--- src/cube/engine_solver.py
+++ src/cube/engine_solver.py
@@ -1,288 +1,223 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import Any, Literal
+
+
+class EdgeType(str, Enum):
+    SEVEN = "_seven_type"
+    THREE = "_three_type"
+    ONE = "_one_type"
+    TOP = "_top_type"
+    FIVE = "_five_type"
+    BOTTOM = "bottum_type"
+
+
+@dataclass(frozen=True)
+class EdgeCase:
+    edge_type: EdgeType
+    white_face: str
+    adjacent_face_color: str
+    sticker_color: str
+
+    def as_legacy(self) -> list[str]:
+        return [
+            self.edge_type.value,
+            self.white_face,
+            self.adjacent_face_color,
+            self.sticker_color,
+        ]
+
+
+COLOR_INDEX = {"g": 0, "r": 1, "b": 2, "o": 3}
+BOTTOM_DELTA_TO_MOVE = {0: ["I"], 1: ["Dp"], 2: ["D2"], 3: ["D"]}
+TOP_DELTA_TO_MOVE = {0: ["I"], 1: ["U"], 2: ["U2"], 3: ["Up"]}
+TOP_EDGE_TYPES = {EdgeType.TOP, EdgeType.ONE}
+BOTTOM_EDGE_TYPES = {EdgeType.FIVE, EdgeType.BOTTOM}
+
+
 class CrossSetupForSolverMixin:
     """
-    This class provides functions used as set up for the CrossSolverMixin class.
+    Setup helpers used by the cross solver.
+
+    These methods classify white-edge cases and compute orientation setup moves.
     """
+
+    def _combo_cases(self) -> list[EdgeCase]:
+        cross_dict = self.identify_cross_edge_type()
+        cases: list[EdgeCase] = []
+        for item in cross_dict:
+            edge_name, by_face = next(iter(item.items()))
+            for face_with_white_edge, (adj_color, sticker_color) in by_face.items():
+                cases.append(
+                    EdgeCase(
+                        edge_type=EdgeType(edge_name),
+                        white_face=face_with_white_edge,
+                        adjacent_face_color=adj_color,
+                        sticker_color=sticker_color,
+                    )
+                )
+        return cases
 
     def combo(self):
         """
-        This is the heart of identifying cross edge types to solve.
-        it returns a list of lists that represent each white t
-
-        This soul feeding this function is found here:
-            `cube_representation.CubeStateMixin.identify_cross_edge_type()`
-
-        Essentially, it tells me the:
-            - type of white edge i have (seven, three, etc)
-            - the face that white edge exists on
-            - the face the contains the colored edge of the white piece
-            - the color of that piece
+        Legacy `combo` shape used by downstream solver code and tests.
         """
 
-        cross_dict = self.identify_cross_edge_type()
+        combos = [case.as_legacy() for case in self._combo_cases()]
+        while len(combos) < 4:
+            combos.append([])
+        return combos[:4]
 
-        _combo = []
-        _combos = [[]] * 4
-        for i, _type in enumerate(cross_dict):
-            for face_with_white_edge in list(
-                cross_dict[i][list(cross_dict[i].keys())[0]].keys()
-            ):
-                _combo.append(list(cross_dict[i].keys())[0])
-                _combo.append(face_with_white_edge)
-                _combo.append(
-                    list(cross_dict[i].values())[0].get(face_with_white_edge)[0]
-                )
-                _combo.append(
-                    list(cross_dict[i].values())[0].get(face_with_white_edge)[1]
-                )
+    @staticmethod
+    def _is_white_or_yellow(color: str) -> bool:
+        return color in {"w", "y"}
 
-        _combos[0] = _combo[0:4]
-        _combos[1] = _combo[4:8]
-        _combos[2] = _combo[8:12]
-        _combos[3] = _combo[12:16]
+    def _compute_layer_delta(
+        self,
+        source: EdgeCase,
+        target: EdgeCase,
+        mode: Literal["target_aware", "both_aware"],
+    ) -> int:
+        if mode == "target_aware":
+            if self._is_white_or_yellow(target.adjacent_face_color):
+                source_ref = source.adjacent_face_color
+                target_ref = target.white_face[0]
+            else:
+                source_ref = source.adjacent_face_color
+                target_ref = target.adjacent_face_color
+            return COLOR_INDEX[source_ref] - COLOR_INDEX[target_ref]
 
-        return _combos
+        if self._is_white_or_yellow(source.adjacent_face_color) and self._is_white_or_yellow(
+            target.adjacent_face_color
+        ):
+            source_ref = source.white_face[0]
+            target_ref = target.white_face[0]
+        elif self._is_white_or_yellow(target.adjacent_face_color):
+            source_ref = source.adjacent_face_color
+            target_ref = target.white_face[0]
+        elif self._is_white_or_yellow(source.adjacent_face_color):
+            source_ref = source.white_face[0]
+            target_ref = target.adjacent_face_color
+        else:
+            source_ref = source.adjacent_face_color
+            target_ref = target.adjacent_face_color
+        return COLOR_INDEX[source_ref] - COLOR_INDEX[target_ref]
+
+    @staticmethod
+    def _compute_sticker_delta(source: EdgeCase, target: EdgeCase) -> int:
+        return COLOR_INDEX[source.sticker_color] - COLOR_INDEX[target.sticker_color]
+
+    @staticmethod
+    def _bottom_move(delta: int) -> list[str]:
+        return BOTTOM_DELTA_TO_MOVE[delta % 4]
+
+    @staticmethod
+    def _top_move(delta: int) -> list[str]:
+        return TOP_DELTA_TO_MOVE[delta % 4]
 
     def seven_three_orientation_delta(self, i, j):
-        g, r, b, o = 0, 1, 2, 3
-        color_mapping = {"g": g, "r": r, "b": b, "o": o}
+        cases = self._combo_cases()
+        source = cases[i]
+        target = cases[j]
 
-        bottum_mapping = {0: ["I"], 1: ["Dp"], 2: ["D2"], 3: ["D"]}
-        top_mapping = {0: ["I"], 1: ["U"], 2: ["U2"], 3: ["Up"]}
+        layer_delta = self._compute_layer_delta(source, target, mode="target_aware")
+        sticker_delta = self._compute_sticker_delta(source, target)
 
-        _combo = self.combo()
-
-        # this is an edge case when we are doing U_delta for one_type/five_type for seven/three
-        if _combo[j][2] == "y" or _combo[j][2] == "w":
-            layer_delta = (
-                color_mapping[_combo[i][2]] - color_mapping[_combo[j][1][0]]
-            )  # need face of one_type/five_type
-        else:
-            layer_delta = color_mapping[_combo[i][2]] - color_mapping[_combo[j][2]]
-
-        sticker_delta = (
-            color_mapping[_combo[i][3]] - color_mapping[_combo[j][3]]
-        )  # an edge type for y/w edge case is not needed because sticker will not be yellow or white for this release on sticker_delta
-
-        tops = ["_top_type", "_one_type"]
-        bottums = ["_five_type", "bottum_type"]
-
-        # D delta
-        if _combo[j][0] in bottums:
-            new_delta = 0
-            # for seven/three to bottum
-            if _combo[j][0] == "bottum_type":
+        if target.edge_type in BOTTOM_EDGE_TYPES:
+            if target.edge_type == EdgeType.BOTTOM:
                 if layer_delta != sticker_delta:
-                    new_delta = (layer_delta - sticker_delta) % 4
-                    return bottum_mapping[new_delta]
-                else:
-                    return [
-                        "I"
-                    ]  # added for case when delta are the same and thus no move needed
-            # for seven and three for five
-            elif _combo[j][0] == "_five_type":
-                new_delta = (layer_delta) % 4
-                return bottum_mapping[new_delta]
-        # U delta
-        # for seven/three to top/one
-        elif _combo[j][0] in tops:
-            new_delta = (layer_delta) % 4
-            return top_mapping[new_delta]
-
-    def combine_seven_three_orientation_delta(self, i, j, k):
-        if (
-            self.seven_three_orientation_delta(i, j) is not None
-            and self.seven_three_orientation_delta(i, k) is not None
-        ):
-            return self.seven_three_orientation_delta(
-                i, j
-            ) + self.seven_three_orientation_delta(i, k)
-        elif self.seven_three_orientation_delta(i, j) is not None:
-            return self.seven_three_orientation_delta(i, j)
-        elif self.seven_three_orientation_delta(i, k) is not None:
-            return self.seven_three_orientation_delta(i, k)
-        else:
-            return ["I"]
-        # this way we can give one type/top/bottum scenario and return combined top/bottum set up moves
-
-    def one_orientation_delta(self, i, j):
-        # anywhere it says "either, not both... it should be in the form of [[alpha], [beta]]... not [alpha, beta]"
-        g, r, b, o = 0, 1, 2, 3
-        color_mapping = {"g": g, "r": r, "b": b, "o": o}
-
-        bottum_mapping = {0: ["I"], 1: ["Dp"], 2: ["D2"], 3: ["D"]}
-
-        _combo = self.combo()
-
-        # this is an edge case when we are doing D_delta for five_type for top/one
-        if (_combo[i][2] == "y" or _combo[i][2] == "w") and (
-            _combo[j][2] == "y" or _combo[j][2] == "w"
-        ):  # one to five case
-            layer_delta = (
-                color_mapping[_combo[i][1][0]] - color_mapping[_combo[j][1][0]]
-            )  # need face because that will tell me layer of one_type/five_type
-        elif _combo[j][2] == "y" or _combo[j][2] == "w":
-            layer_delta = (
-                color_mapping[_combo[i][2]] - color_mapping[_combo[j][1][0]]
-            )  # need face because that will tell me layer of one_type/five_type
-
-        elif _combo[i][2] == "y" or _combo[i][2] == "w":
-            layer_delta = (
-                color_mapping[_combo[i][1][0]] - color_mapping[_combo[j][2]]
-            )  # need face because that will tell me layer of one_type/five_type
-        else:
-            layer_delta = color_mapping[_combo[i][2]] - color_mapping[_combo[j][2]]
-
-        # an edge type for y/w edge case is not needed because sticker will not be yellow or white for this release on sticker_delta
-        # sticker_delta = (
-        #     color_mapping[_combo[i][3]] - color_mapping[_combo[j][3]]
-        # )
-
-        # tops = ["_top_type", "_one_type"]
-        bottums = ["_five_type", "bottum_type"]
-
-        # D delta
-        res = ["I"]
-        if _combo[j][0] in bottums:
-            new_delta = 0
-            # for one to bottum (the logic here will be to clear the bottum (provide both one move away if already there, else do nothing, and then do F or Fp, provide both and let tree decide))
-            if _combo[j][0] == "bottum_type":
-                if layer_delta == 0:
-                    #                 new_delta = (layer_delta - sticker_delta)%4
-                    #                 return bottum_mapping[new_delta]
-                    res = ["D", "Dp"]  # either, not both
-                else:
-                    res = [
-                        "I"
-                    ]  # added for case when delta are the same and thus no move needed; dont need to do anything becuase it will happen in seven/three
-            # just bring to right location
-            # for top and one to five (bring under when doing F swing)
-            elif _combo[j][0] == "_five_type":
-                new_delta = (layer_delta) % 4
-                res = bottum_mapping[new_delta]
-            # just bring it under it, the solver will do both moves aand add to trie and see what is better
-        return res
-        # no U delta
-
-    def top_orientation_delta(self, i, j):
-        g, r, b, o = 0, 1, 2, 3
-        color_mapping = {"g": g, "r": r, "b": b, "o": o}
-
-        bottum_mapping = {0: ["I"], 1: ["Dp"], 2: ["D2"], 3: ["D"]}
-
-        _combo = self.combo()
-
-        # this is an edge case when we are doing D_delta for five_type for top/one
-        if _combo[j][2] == "y" or _combo[j][2] == "w":
-            layer_delta = (
-                color_mapping[_combo[i][2]] - color_mapping[_combo[j][1][0]]
-            )  # need face because that will tell me layer of one_type/five_type
-        else:
-            layer_delta = color_mapping[_combo[i][2]] - color_mapping[_combo[j][2]]
-
-        sticker_delta = (
-            color_mapping[_combo[i][3]] - color_mapping[_combo[j][3]]
-        )  # an edge type for y/w edge case is not needed because sticker will not be yellow or white for this release on sticker_delta
-
-        # tops = ["_top_type", "_one_type"]
-        bottums = ["_five_type", "bottum_type"]
-
-        # D delta
-        res = ["I"]
-        if _combo[j][0] in bottums:
-            new_delta = 0
-            # for top to bottum
-            if _combo[j][0] == "bottum_type":
-                if layer_delta != sticker_delta:
-                    new_delta = (
-                        layer_delta - sticker_delta
-                    ) % 4  # for now, keeping same logic of seven/three and moving to corrrect orientation, and not following one_type logic and just moving to either side... might revist if need be?
-                    res = bottum_mapping[new_delta]
-                else:
-                    res = [
-                        "I"
-                    ]  # added for case when delta are the same and thus no move needed
-            # just bring to right location
-            # for top and one to five
-            elif _combo[j][0] == "_five_type":
-                new_delta = (layer_delta) % 4
-                res = bottum_mapping[new_delta]
-            # just bring it under it, the solver will do both moves aand add to trie and see what is better
-        return res
-        # no U delta
-
-    def five_orientation_delta(self, i, j):
-        # anywhere it says "either, not both... it should be in the form of [[alpha], [beta]]... not [alpha, beta]"
-        g, r, b, o = 0, 1, 2, 3
-        color_mapping = {"g": g, "r": r, "b": b, "o": o}
-
-        # bottum_mapping = {0: ["I"], 1: ["Dp"], 2: ["D2"], 3: ["D"]}
-        top_mapping = {0: ["I"], 1: ["U"], 2: ["U2"], 3: ["Up"]}
-
-        _combo = self.combo()
-
-        # this is an edge case when we are doing D_delta for five_type for top/one
-        if (_combo[i][2] == "y" or _combo[i][2] == "w") and (
-            _combo[j][2] == "y" or _combo[j][2] == "w"
-        ):  # one to five case
-            layer_delta = (
-                color_mapping[_combo[i][1][0]] - color_mapping[_combo[j][1][0]]
-            )  # need face because that will tell me layer of one_type/five_type
-        elif _combo[j][2] == "y" or _combo[j][2] == "w":
-            layer_delta = (
-                color_mapping[_combo[i][2]] - color_mapping[_combo[j][1][0]]
-            )  # need face because that will tell me layer of one_type/five_type
-
-        elif _combo[i][2] == "y" or _combo[i][2] == "w":
-            layer_delta = (
-                color_mapping[_combo[i][1][0]] - color_mapping[_combo[j][2]]
-            )  # need face because that will tell me layer of one_type/five_type
-        else:
-            layer_delta = color_mapping[_combo[i][2]] - color_mapping[_combo[j][2]]
-
-        # an edge type for y/w edge case is not needed because sticker will not be yellow or white for this release on sticker_delta
-        # sticker_delta = (
-        #     color_mapping[_combo[i][3]] - color_mapping[_combo[j][3]]
-        # )
-
-        tops = ["_top_type", "_one_type"]
-        # bottums = ["_five_type", "bottum_type"]
-
-        # no D delta
-        res = ["I"]
-        # U delta
-        if _combo[j][0] in tops:
-            new_delta = (layer_delta) % 4
-            res = top_mapping[new_delta]
-
-        return res
-
-    def bottum_orientation_delta(self):
-        # only use this function when cross is oriented
-
-        if self.cross_oriented():
-            g, r, b, o = 0, 1, 2, 3
-            color_mapping = {"g": g, "r": r, "b": b, "o": o}
-
-            bottum_mapping = {0: ["I"], 1: ["Dp"], 2: ["D2"], 3: ["D"]}
-
-            _combo = self.combo()
-
-            sticker_delta = (
-                color_mapping[self.cube_state["green"][5]]
-                - color_mapping[self.cube_state["green"][8]]
-            )  # an edge type for y/w edge case is not needed because sticker will not be yellow or white for this release on sticker_delta
-
-            return bottum_mapping[sticker_delta]
-
-        else:
+                    return self._bottom_move(layer_delta - sticker_delta)
+                return ["I"]
+            if target.edge_type == EdgeType.FIVE:
+                return self._bottom_move(layer_delta)
             return None
 
+        if target.edge_type in TOP_EDGE_TYPES:
+            return self._top_move(layer_delta)
+        return None
 
+    def combine_seven_three_orientation_delta(self, i, j, k):
+        left = self.seven_three_orientation_delta(i, j)
+        right = self.seven_three_orientation_delta(i, k)
+
+        merged: list[str] = []
+        if left is not None:
+            merged.extend(left)
+        if right is not None:
+            merged.extend(right)
+        return merged if merged else ["I"]
+
+    def one_orientation_delta(self, i, j):
+        # "either, not both" is represented as e.g. ["D", "Dp"]
+        cases = self._combo_cases()
+        source = cases[i]
+        target = cases[j]
+
+        layer_delta = self._compute_layer_delta(source, target, mode="both_aware")
+        result = ["I"]
+
+        if target.edge_type in BOTTOM_EDGE_TYPES:
+            if target.edge_type == EdgeType.BOTTOM:
+                if layer_delta == 0:
+                    result = ["D", "Dp"]
+                else:
+                    result = ["I"]
+            elif target.edge_type == EdgeType.FIVE:
+                result = self._bottom_move(layer_delta)
+        return result
+
+    def top_orientation_delta(self, i, j):
+        cases = self._combo_cases()
+        source = cases[i]
+        target = cases[j]
+
+        layer_delta = self._compute_layer_delta(source, target, mode="target_aware")
+        sticker_delta = self._compute_sticker_delta(source, target)
+        result = ["I"]
+
+        if target.edge_type in BOTTOM_EDGE_TYPES:
+            if target.edge_type == EdgeType.BOTTOM:
+                if layer_delta != sticker_delta:
+                    result = self._bottom_move(layer_delta - sticker_delta)
+                else:
+                    result = ["I"]
+            elif target.edge_type == EdgeType.FIVE:
+                result = self._bottom_move(layer_delta)
+        return result
+
+    def five_orientation_delta(self, i, j):
+        # "either, not both" is represented as e.g. ["F", "Fp"]
+        cases = self._combo_cases()
+        source = cases[i]
+        target = cases[j]
+
+        layer_delta = self._compute_layer_delta(source, target, mode="both_aware")
+        result = ["I"]
+        if target.edge_type in TOP_EDGE_TYPES:
+            result = self._top_move(layer_delta)
+        return result
+
+    def bottom_orientation_delta(self):
+        # only use this function when cross is oriented
+        if not self.cross_oriented():
+            return None
+
+        sticker_delta = (
+            COLOR_INDEX[self.cube_state["green"][5]]
+            - COLOR_INDEX[self.cube_state["green"][8]]
+        )
+        return BOTTOM_DELTA_TO_MOVE[sticker_delta % 4]
+
+    def bottum_orientation_delta(self):
+        # backward-compatible typo alias
+        return self.bottom_orientation_delta()
+
+
+@dataclass
 class TreeNode:
     """
-    This class is used to support the CrossSolver class.
+    Support node used by the cross-solver search tree.
     """
 
-    def __init__(self, val):
-        self.val = val
-        self.children = []
+    val: Any
+    children: list["TreeNode"] = field(default_factory=list)
